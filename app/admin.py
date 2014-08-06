1ca6c05d9e68ddd23a763c19acef028bd1507fd8
Kevin Ladenheim <kpladenheim@gmail.com>
2014-08-05 22:53:59 -0400

    new static assets

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,30 @@
 from django.contrib import admin
+from models import Nutritionist, Consultation, Credential, Demographic, Specialty, Availability
 import reversion
 
-# class YourModelAdmin(reversion.VersionAdmin):
-#     pass
+admin.site.register(Consultation)
+admin.site.register(Credential)
+admin.site.register(Demographic)
+admin.site.register(Specialty)
+admin.site.register(Availability)
 
-#admin.site.register(<your model here>, YourModelAdmin)
+
+class AvailabilityInline(admin.StackedInline):
+   model = Availability
+   extra = 1
+   max_num = 1
+
+
+class CredentialInline(admin.StackedInline):
+   model = Credential.nutritionist.through
+   extra = 1
+   max_num = 5
+
+
+class NutritionistAdmin(admin.ModelAdmin):
+   inlines = [
+      AvailabilityInline,
+      CredentialInline
+   ] # These are not showing up at all, but the code is being eval, wtf?
+
+admin.site.register(Nutritionist, NutritionistAdmin)
--- app/models.py
+++ app/models.py
@@ -1,5 +1,151 @@
 from django.db import models
 from django_extensions.db.models import AutoSlugField
-
 from model_utils.models import TimeStampedModel
-# Create your models here.
+
+
+class Nutritionist(TimeStampedModel):
+
+   NUTRITIONIST_TYPES = (
+       ('dietician' , 'Registered Dietician'),
+       ('certified_nutrtion_specialist' , 'Certified nutrition specialist'),
+       ('certified_clinical_nutritionist' , 'Certified clinical nutritionist'),
+       ('diplomat' , 'Diplomat of the American Clinical Board of Nutrition'),
+       ('holistic_nutritionist' , 'Holistic nutritionist'),
+       ('certified_health_coach' , 'Certified health coach'),
+       ('certified_nutritionist' , 'Certified nutritionist'),
+       ('certified_nutrition_consultant' , 'Certified nutrition consultant'),
+     )
+
+   MEETING_TYPES = (
+       ('in_person' , 'In Person'),
+       ('phone_call' , 'Phone Call'),
+       ('video_call' , 'Video Call'),
+     )
+
+   first_name = models.CharField(max_length=255)
+   last_name = models.CharField(max_length=255)
+   nutritionist_type = models.CharField(max_length=255, choices=NUTRITIONIST_TYPES)
+   meeting_type = models.CharField(max_length=255, choices=MEETING_TYPES)
+   tag_phrase = models.CharField(max_length=2048)
+   city = models.CharField(max_length=255)
+   state = models.CharField(max_length=255)
+   zip = models.CharField(max_length=255)
+   users = models.ManyToManyField('account.User', related_name='nutritionist_users', null=True, blank=True)
+   consultation_description = models.CharField(max_length=4096)
+   skype_name = models.CharField(max_length=255)
+
+   def get_full_name(self):
+        return unicode(self.first_name) + " " + unicode(self.last_name)
+
+   def __unicode__(self):
+        return self.get_full_name()
+
+
+class Consultation(TimeStampedModel):
+   user = models.ForeignKey('account.User', related_name='user_consultation', null=True, blank=True)
+   nutritionist = models.ForeignKey('Nutritionist', related_name='nutritionist_consultation', null=True, blank=True)
+   consultation = models.DateTimeField()
+
+   def __unicode__(self):
+        return self.user.get_full_name() + " and " + self.nutritionist.get_full_name() + " on " + unicode(self.consultation)
+
+
+class Credential(TimeStampedModel):
+   CREDENTIAL_TYPE = (
+      ('degree', 'Degree'),
+      ('license', 'License'),
+      ('course', 'Course'),
+   )
+
+   type = models.CharField(max_length=255, choices=CREDENTIAL_TYPE)
+   description = models.CharField(max_length=1024)
+   nutritionist = models.ManyToManyField('Nutritionist', related_name='nutritionist_credential', null=True, blank=True)
+
+   def __unicode__(self):
+        return self.nutritionist.get_full_name() + unicode(self.type) + " " + unicode(self.description)
+
+
+class Demographic(TimeStampedModel):
+   DEMOGRAPHIC_TYPE = (
+      ('adult', 'Adult'),
+      ('senior', 'Senior'),
+      ('christian', 'Christian'),
+      ('muslim', 'Muslim'),
+      ('jewish', 'Jewlish'),
+      ('buddhists', 'Buddhists'),
+      ('african_american', 'African American'),
+      ('latino_american', 'Latino American'),
+      ('asian_american', 'Asian American'),
+   )
+
+   type = models.CharField(max_length=255, choices=DEMOGRAPHIC_TYPE)
+   nutritionist = models.ManyToManyField('Nutritionist', related_name='nutritionist_demographic', null=True, blank=True)
+
+   def __unicode__(self):
+        return unicode(self.type)
+
+
+class Specialty(TimeStampedModel):
+   SPECIALTY_TYPE = (
+      ('anger_management', 'Anger Management'),
+      ('aspergers', 'Asperger\'s'),
+      ('divorce', 'Divorce'),
+      ('domestic_violence', 'Domestic Violence'),
+      ('relationships', 'Relationships'),
+      ('spirituality', 'Spirituality'),
+      ('stress', 'Stress'),
+   )
+
+   type = models.CharField(max_length=255, choices=SPECIALTY_TYPE)
+   nutritionist = models.ManyToManyField('Nutritionist', related_name='nutritionist_specialty', null=True, blank=True)
+
+   def __unicode__(self):
+        return unicode(self.type)
+
+
+class Availability(TimeStampedModel):
+
+   monday_early = models.BooleanField(default=False)
+   monday_afternoon = models.BooleanField(default=False)
+   monday_evening = models.BooleanField(default=False)
+
+   tuesday_early = models.BooleanField(default=False)
+   tuesday_afternoon = models.BooleanField(default=False)
+   tuesday_evening = models.BooleanField(default=False)
+
+   wednesday_early = models.BooleanField(default=False)
+   wednesday_afternoon = models.BooleanField(default=False)
+   wednesday_evening = models.BooleanField(default=False)
+
+   thursday_early = models.BooleanField(default=False)
+   thursday_afternoon = models.BooleanField(default=False)
+   thursday_evening = models.BooleanField(default=False)
+
+   friday_early = models.BooleanField(default=False)
+   friday_afternoon = models.BooleanField(default=False)
+   friday_evening = models.BooleanField(default=False)
+
+   saturday_early = models.BooleanField(default=False)
+   saturday_afternoon = models.BooleanField(default=False)
+   saturday_evening = models.BooleanField(default=False)
+
+   sunday_early = models.BooleanField(default=False)
+   sunday_afternoon = models.BooleanField(default=False)
+   sunday_evening = models.BooleanField(default=False)
+
+   nutritionist = models.ForeignKey('Nutritionist', related_name='nutritionist_availability', null=True, blank=True)
+
+   def __unicode__(self):
+        return ("  M " + unicode(self.monday_early) + " " + unicode(self.monday_afternoon) + " " + unicode(self.monday_evening) + " " + \
+   "  T " + unicode(self.tuesday_early) + " " +  unicode(self.tuesday_afternoon) + " " + unicode(self.tuesday_evening) + " " + \
+   "  W " + unicode(self.wednesday_early) + " " + unicode(self.wednesday_afternoon) + " " + unicode(self.wednesday_evening) + " " + \
+   "  TR " + unicode(self.thursday_early) + " " + unicode(self.thursday_afternoon) + " " + unicode(self.thursday_evening) + " " + \
+   "  F " + unicode(self.friday_early) + " " + unicode(self.friday_afternoon) + " " + unicode(self.friday_evening) + " " + \
+   "  S " + unicode(self.saturday_early) + " " + unicode(self.saturday_afternoon) + " " + unicode(self.saturday_evening) + " " + \
+   "  S " + unicode(self.sunday_early) + " " + unicode(self.sunday_afternoon) + " " + unicode(self.sunday_evening))
+
+'''
+class (TimeStampedModel):
+   def __unicode__(self):
+        return unicode(self.)
+'''

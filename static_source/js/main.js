8c85c1edcf3015067d5a9716d9deee2b9924c862
Kevin Ladenheim <kpladenheim@gmail.com>
2014-09-08 16:28:06 -0400

    styling changes

--- static_source/js/main.js
+++ static_source/js/main.js
@@ -1,4 +1,8 @@
-$(document).ready(function(){
-    
-
-});
+function populate_calendar_selects(dateText, inst, month_id, day_id, year_id) {
+   console.log("populate");
+   date = dateText.match(/^(\d{2})\s(\d{2})\s(\d{4})/);
+   console.log(date);
+   $('#' + month_id).val(parseInt(date[1]));
+   $('#' + day_id).val(parseInt(date[2]));
+   $('#' + year_id).val(parseInt(date[3]));
+}
